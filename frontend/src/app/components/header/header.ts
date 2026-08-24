581d092a7d01598d19aea63777af78d7a3f7a63e
soteiro <diego_sarq@hotmail.com>
2026-08-24 16:16:19 -0400

    feat: update header component to include logo and router link

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink} from '@angular/router'
 import { useToggle } from '../../utils/use-toggle'
 import { LucideMenu } from '@lucide/angular'
 @Component({
   selector: 'app-header',
-  imports: [LucideMenu],
+  imports: [LucideMenu, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
